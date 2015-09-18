2e9c88367d14ea26efe0ff13ab46346bd32bbc45
e.bashtan <bashtan.e.a@gmail.com>
2015-09-18 11:44:28 +0300

    Add logging

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ var url = require('url');
 class Server {
 	constructor() {
 		this._clients = {};
+		this._clientsCount = 0;
 	}
 
 	_register(id, cl_id, req,res, start_date) {
 		if(!this._clients)
 			this._clients = {};
 		logger.save('db', {cl_id:cl_id, id:id, type:'open',start_date: start_date});
+		this._clientsCount ++;
 
-		this._clients[cl_id] = { req: req, res:res };
+		this._clients[cl_id] = { req: req, res:res, cl_id: cl_id, name: this._getName() };
 		this._clients[cl_id].start_date = start_date;
 
 		res.writeHead(200, {
@@ -25,18 +27,50 @@ class Server {
 
 		req.on('close', () =>  this._unregister(cl_id, id));
 
-		this._clients[cl_id].int = setInterval(this._sendMess.bind(this, this._clients[cl_id]), 2000);
-	}
 
+	}
+	_getName(){
+		var names = ['Алексий',
+			'Алина',
+			'Алена',
+			'Маруся',
+			'Афоний',
+			'Всеволод'
+			, 'Феодора'
+			, 'Аксинья'
+			, 'Габриела'
+			, 'Парамон'
+			, 'Ильгизар'
+			, 'Генрих'
+			, 'Евпраксия'
+			, 'Ясон'
+			, 'Искандер'];
+		var max = names.length, min = 0;
+		return names[Math.floor(Math.random() * (max - min)) + min];
+
+	}
 	_sendMess(cl){
-		var msg = Math.random();
-		cl.res.write("data: "+ msg + "\n\n");
+		if(this._clientsCount > 0){
+			var msg = Math.random();
+			var act = [];
+			for(cl in this._clients){
+				act.push(this._clients[cl].name)
+			}
+
+			for(cl in this._clients){
+				console.info('Message for -> '+this._clients[cl].name + ' active users('+this._clientsCount+') ['+act.join(', ')+']')
+				this._clients[cl].res.write("data: Привет "+this._clients[cl].name + ' #' + msg + "\n\n");
+			}
+
+		}
 	}
 
 	_unregister(cl_id, id) {
+		this._clientsCount --;
+
 		if(!this._clients[cl_id]) return logger.log('No such connection');
 		logger.save('db', {cl_id:cl_id, id:id, type:'close',start_date: this._clients[cl_id].start_date});
-		clearInterval(this._clients[cl_id].int);
+
 		delete this._clients[cl_id];
 	}
 
@@ -48,7 +82,7 @@ class Server {
 	<script type="text/javascript"> \
 		    var source = new EventSource("/events/"); \
 		    source.onmessage = function(e) { \
-		        document.body.innerHTML += e.data + "<br>"; \
+		        document.body.innerHTML = e.data + "<br>"; \
 		    }; \
 </script> </body> </html>';
 
@@ -61,6 +95,7 @@ class Server {
 			var id = Math.random();
 			this._register(id, cl_id, req, res, new Date());
 		});
+		setInterval(this._sendMess.bind(this), 2000);
 		app.listen(3001);
 	}
 	stop() {
