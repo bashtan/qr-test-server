1e35627f7f9164d9b7031941b654df30a03470d1
e.bashtan <bashtan.e.a@gmail.com>
2015-10-20 17:59:52 +0300

    SSE test (reverted from commit 5ec8f7effd8d920921a31a3916049fe7d52c0c7e)

--- server.js
+++ server.js
@@ -1,102 +1,53 @@
-var logger	= require('logger');
-var express = require('express');
-var app = express();
+var logger			= require('logger');
+var WebSocketServer = new require('ws');
+var webSocketServer = new WebSocketServer.Server({port: 3001});
 var url = require('url');
 
 class Server {
 	constructor() {
 		this._clients = {};
-		this._clientsCount = 0;
 	}
 
-	_register(id, cl_id, req,res, start_date) {
-		if(!this._clients)
-			this._clients = {};
+	_register(id, cl_id, ws, start_date) {
 		logger.save('db', {cl_id:cl_id, id:id, type:'open',start_date: start_date});
-		this._clientsCount ++;
 
-		this._clients[cl_id] = { req: req, res:res, cl_id: cl_id, name: this._getName() };
+		this._clients[cl_id] = { ws: ws };
 		this._clients[cl_id].start_date = start_date;
 
-		res.writeHead(200, {
-			'Content-Type': 'text/event-stream',  // <- Important headers
-			'Cache-Control': 'no-cache',
-			'Connection': 'keep-alive'
+		ws.on('close', () =>  this._unregister(cl_id, id));
+		ws.on('error', () =>  this._unregister(cl_id, id));
+		ws.on('message', event => {
+			try{
+				if(event == 'end') logger.save('db', {cl_id:cl_id,id:id, type:'message'});
+				this._clients[cl_id].ws.send(event)
+			}catch (e){
+				this._unregister(cl_id);
+			}
 		});
-		res.write('\n');
-
-		req.on('close', () =>  this._unregister(cl_id, id));
-
-
-	}
-	_getName(){
-		var names = ['Алексий',
-			'Алина',
-			'Алена',
-			'Маруся',
-			'Афоний',
-			'Всеволод'
-			, 'Феодора'
-			, 'Аксинья'
-			, 'Габриела'
-			, 'Парамон'
-			, 'Ильгизар'
-			, 'Генрих'
-			, 'Евпраксия'
-			, 'Ясон'
-			, 'Искандер'];
-		var max = names.length, min = 0;
-		return names[Math.floor(Math.random() * (max - min)) + min];
-
-	}
-	_sendMess(cl){
-		if(this._clientsCount > 0){
-			var msg = Math.random();
-			var act = [];
-			for(cl in this._clients){
-				act.push(this._clients[cl].name)
-			}
-
-			for(cl in this._clients){
-				console.info('Message for -> '+this._clients[cl].name + ' active users('+this._clientsCount+') ['+act.join(', ')+']')
-				this._clients[cl].res.write("data: Привет "+this._clients[cl].name + ' #' + msg + "\n\n");
-			}
-
-		}
 	}
 
 	_unregister(cl_id, id) {
-		this._clientsCount --;
-
 		if(!this._clients[cl_id]) return logger.log('No such connection');
 		logger.save('db', {cl_id:cl_id, id:id, type:'close',start_date: this._clients[cl_id].start_date});
-
 		delete this._clients[cl_id];
 	}
 
 	start() {
 		logger.save('all', 'Server started');
-		this._clients = null;
-		var template = ' \
-<!DOCTYPE html> <html> <body> \
-	<script type="text/javascript"> \
-		    var source = new EventSource("/events/"); \
-		    source.onmessage = function(e) { \
-		        document.body.innerHTML = e.data + "<br>"; \
-		    }; \
-</script> </body> </html>';
 
-		app.get('/', function(req, res) {
-			res.send(template);  // <- Return the static template above
-		});
 
-		app.get('/events/', (req, res) => {
+		webSocketServer.on('error', ws=> {});
+		webSocketServer.on('connection', ws=> {
 			var cl_id = Math.random();
 			var id = Math.random();
-			this._register(id, cl_id, req, res, new Date());
+			try{
+				var location = url.parse(ws.upgradeReq.url, true);
+				id = location.query.id? location.query.id:Math.random();
+			}catch(e){
+				id = Math.random();
+			}
+			this._register(id, cl_id, ws, new Date());
 		});
-		setInterval(this._sendMess.bind(this), 2000);
-		app.listen(3001);
 	}
 	stop() {
 	}
